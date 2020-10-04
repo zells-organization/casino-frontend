1d1e3f817534cce55e2d7d533ecc3e7b5fae487a
Yaroslav <yarok1999@gmail.com>
2020-10-04 14:55:45 +0200

    changed App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,50 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <v-app>
+      <v-content v-if="loggedIn===null">
+        <v-container fill-height>
+          <v-layout align-center justify-center>
+            <v-flex>
+              <div class="text-xs-center">
+                <div class="headline my-5">Loading...</div>
+                <v-progress-circular size="100" indeterminate color="primary"></v-progress-circular>
+              </div>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <router-view v-else />
+      <NotificationsManager></NotificationsManager>
+    </v-app>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import { Component, Vue } from 'vue-property-decorator';
+import NotificationsManager from '@/components/NotificationsManager.vue';
+import { readIsLoggedIn } from '@/store/main/getters';
+import { dispatchCheckLoggedIn } from '@/store/main/actions';
 
-export default Vue.extend({
-  name: 'App',
+@Component({
+  components: {
+    NotificationsManager,
+  },
+})
 
-  data: () => ({
-    //
-  }),
-});
+export default class App extends Vue {
+
+  get loggedIn() {
+    return readIsLoggedIn(this.$store);
+  }
+
+  public async created() {
+    await dispatchCheckLoggedIn(this.$store);
+  }
+
+}
+
 </script>
+
+
+
+

564e70da7e67fdeaf033a66b338a37a9b9879d0d
sa1exx <sandrez.spartanell@gmail.com>
2020-10-27 15:33:21 +0200

    dominoes removed text-align

--- src/views/main/games/dominoes/Dominoes.vue
+++ src/views/main/games/dominoes/Dominoes.vue
@@ -21,7 +21,7 @@ export default {
   font-family: 'Avenir', Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   width: 100%;
   height: 100%;
 }

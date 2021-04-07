b66238e18e1ec3297796ec9c6eaa44efb14a0e97
Kostya <kostia1234455@gmail.com>
2021-04-07 18:50:32 +0300

    Full screen background size

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,13 +1,10 @@
 <style lang="sass" scoped>
 .reset-password-container
   background: url("../assets/reset-password-background-image.jpg") no-repeat
-    -moz-background-size: 100%
-    /* Firefox 3.6+ */
-    -webkit-background-size: 100%
-    /* Safari 3.1+ и Chrome 4.0+ */
-    -o-background-size: 100%
-    /* Opera 9.6+ */
-    background-size: 100%
+  -moz-background-size: 100% /* Firefox 3.6+ */
+  -webkit-background-size: 100% /* Safari 3.1+ и Chrome 4.0+ */
+  -o-background-size: 100% /* Opera 9.6+ */
+  //background-size: 100%
 </style>
 
 <template>
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,13 +1,10 @@
 <style lang="sass" scoped>
 .login-container
   background: url("../assets/signup-background-image.jpg") no-repeat
-    -moz-background-size: 100%
-    /* Firefox 3.6+ */
-    -webkit-background-size: 100%
-    /* Safari 3.1+ и Chrome 4.0+ */
-    -o-background-size: 100%
-    /* Opera 9.6+ */
-    background-size: 100%
+  -moz-background-size: 100% /* Firefox 3.6+ */
+  -webkit-background-size: 100% /* Safari 3.1+ и Chrome 4.0+ */
+  -o-background-size: 100% /* Opera 9.6+ */
+  //background-size: 100%
 </style>
 
 <template>
